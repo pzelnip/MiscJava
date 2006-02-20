84a843f167cedfe3f818adf77863f00a177262ce
aparkin <aparkin>
2006-02-20 20:30:59 +0000

    Initial revision

---
+++ CharString.java
@@ -0,0 +1,41 @@
+/*
+
+Interesting exploration of Java's implicit casting rules (or lack thereof) between strings
+and characters.
+
+*/
+
+public class CharString 
+{
+	public static void main (String [] args)
+	{
+		String bar;
+
+		bar = "A";
+
+		Character a = new Character ('A');
+
+		// explicitly convert the character to a string, works fine
+		// but seems redundant
+		System.out.println ((bar.equals (a.toString()) ? "true" : "false"));
+
+		// however if we just pass a Character, the comparison fails, and
+		// I have no idea why
+		System.out.println ((bar.equals (a) ? "true" : "false"));
+
+		// and of course direct comparison fails, since Java does strict
+		// type checking, and Character and String are different types.
+		// thus the following line will not compile
+		// System.out.println ((bar == a ? "true" : "false"));
+		
+		// Now where it gets really stupid:
+
+		// The String class' constructor is not overloaded to accept a
+		// character object, and thus the following line is illegal
+		// String foo = new String (a);
+
+		// To do the above you need to call the toString() method of
+		// the characcter like so:
+		String foo = new String (a.toString());
+	}
+}
