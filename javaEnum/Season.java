7f921e44e9687ecb25d9c5f64a239fdfa70b4256
Adam Parkin <aparkin@seekersolutions.com>
2013-04-04 14:55:51 -0700

    Fixed whitespace in enum example

--- javaEnum/Season.java
+++ javaEnum/Season.java
@@ -1,25 +1,22 @@
-public enum Season { 
-	WINTER ("Brrr, it's cold!"),
-	SPRING ("Getting a bit warmer..."),
-	SUMMER ("The sun is shining..."), 
-	FALL ("Look at the leaves turning colour..");
+public enum Season {
+    WINTER("Brrr, it's cold!"), 
+    SPRING("Getting a bit warmer..."), 
+    SUMMER("The sun is shining..."), 
+    FALL("Look at the leaves turning colour..");
 
-	private final String msg;
+    private final String msg;
 
-	Season (String sIn)
-	{
-		this.msg = sIn;
-	}
+    Season(String sIn) {
+        this.msg = sIn;
+    }
 
-	public String toString()
-	{
-		// name().toString() gets us the enum constant (ex: WINTER)
-		String s = name().toString(); 
+    public String toString() {
+        // name().toString() gets us the enum constant (ex: WINTER)
+        String s = name().toString();
 
-		// capitalize first letter & add custom message
-		s = s.charAt(0) + s.substring(1).toLowerCase() + 
-			" - " + msg;	
+        // capitalize first letter & add custom message
+        s = s.charAt(0) + s.substring(1).toLowerCase() + " - " + msg;
 
-		return s;
-	}
+        return s;
+    }
 }
--- javaEnum/Test.java
+++ javaEnum/Test.java
@@ -1,53 +1,49 @@
-public class Test
-{
-	public static void main (String [] args)
-	{
-		new Test().runMe();
-	}
+public class Test {
+    public static void main(String[] args) {
+        new Test().runMe();
+    }
 
-	public void runMe()
-	{
-		Season someSeason = getSeason();
+    public void runMe() {
+        Season someSeason = getSeason();
 
-		System.out.println ("Printing out the season we got from getSeason...");
-		System.out.println (someSeason);
+        System.out.println("Printing out the season we got from getSeason...");
+        System.out.println(someSeason);
 
-		// note that we can switch on Enums.
-		switch (someSeason)
-		{
-			case WINTER:
-				// do something for winter...
-				break;
-			case SUMMER:
-				// do something for summer...
-				break;
-			case FALL:
-				// do something for fall...
-				break;
-			case SPRING:
-				// do something for spring...
-				break;
-			default:
-				System.out.println ("Unknown Season!");
-		}
+        // note that we can switch on Enums.
+        switch (someSeason) {
+        case WINTER:
+            // do something for winter...
+            break;
+        case SUMMER:
+            // do something for summer...
+            break;
+        case FALL:
+            // do something for fall...
+            break;
+        case SPRING:
+            // do something for spring...
+            break;
+        default:
+            System.out.println("Unknown Season!");
+        }
 
         Season aSeason = Season.WINTER;
 
         if (aSeason == Season.WINTER) {
-            System.out.println ("aSeason is WINTER!");
+            System.out.println("aSeason is WINTER!");
         } else {
-            System.out.println ("aSeason is NOT winter");
+            System.out.println("aSeason is NOT winter");
         }
 
-		// we can also iterate over all values in an enum
-		System.out.println ("Iterating over all seasons...");
-		for (Season s : Season.values())
-			System.out.println (s);
-	}
+        // we can also iterate over all values in an enum
+        System.out.println("Iterating over all seasons...");
+        for (Season s : Season.values()) {
+            System.out.println(s);
+        }
+    }
 
-	// example showing returning an enum type from a method
-	public Season getSeason ()
-	{
-		return Season.WINTER; 
-	}
+    // example showing returning an enum type from a method
+    public Season getSeason() {
+        return Season.WINTER;
+    }
 }
